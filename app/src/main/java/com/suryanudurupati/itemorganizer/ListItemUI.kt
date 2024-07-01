7e293deb3da6ed52a5da1abb3c03c2d2f85a4f9f
Surya Nudurupati <nudurupatisurya@gmail.com>
2024-07-01 14:46:14 -0700

    UI V1

--- app/src/main/java/com/suryanudurupati/itemorganizer/ListItemUI.kt
+++ app/src/main/java/com/suryanudurupati/itemorganizer/ListItemUI.kt
@@ -1,23 +1,21 @@
 package com.suryanudurupati.itemorganizer
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,22 +26,37 @@ import com.suryanudurupati.itemorganizer.ui.theme.ItemOrganizerTheme
 @Composable
 fun ListItemUI(
     modifier: Modifier = Modifier,
-    listId: Int,
-    ids: List<Int>,
-    names: List<String>
+    listIds: List<Int>,
+    ids: List<List<Int>>,
+    names: List<List<String>>
 ) {
-    Column(modifier = modifier.padding(10.dp)) {
-        Row {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                ListIdUI(listId = listId.toString())
-            }
-            LazyColumn(userScrollEnabled = true) {
-                items(ids.size) { index ->
-                    Row {
-                        Text(text = ids[index].toString(), modifier = modifier.padding(horizontal = 25.dp))
-                        Text(text = names[index], modifier = modifier.padding(horizontal = 25.dp))
+    var height by remember { mutableStateOf(50.dp) }
+    Row(modifier = modifier.fillMaxWidth()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            HeaderText(text = "List ID", modifier = Modifier.padding(horizontal = 10.dp))
+            LazyColumn() {
+                items(listIds.size) { index ->
+                    Row(modifier = Modifier.height(height)) {
+                        ListIdText(listId = listIds[index].toString())
                     }
-                    Spacer(modifier = Modifier.height(4.dp))
+                }
+            }
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            HeaderText(text = "ID", modifier = Modifier.padding(horizontal = 25.dp).onGloballyPositioned { layoutCoordinates ->
+                height = layoutCoordinates.size.height.dp
+            })
+            LazyColumn {
+                items(ids.size) { index ->
+                    ItemRow(item = ids[index].map { it.toString() })
+                }
+            }
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            HeaderText(text = "Name", modifier = Modifier.padding(horizontal = 25.dp))
+            LazyColumn {
+                items(ids.size) { index ->
+                    ItemRow(item = names[index])
                 }
             }
         }
@@ -51,7 +64,19 @@ fun ListItemUI(
 }
 
 @Composable
-fun ListIdUI(modifier: Modifier = Modifier, listId: String) {
+fun ItemRow(modifier: Modifier = Modifier, item: List<String>){
+    Column {
+        for (i in item.indices){
+            Row {
+                Text(item[i])
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ListIdText(modifier: Modifier = Modifier, listId: String) {
     Column(modifier = Modifier.padding(horizontal = 10.dp)) {
         Text(listId, modifier = modifier, fontWeight = FontWeight.Bold)
     }
@@ -69,11 +94,7 @@ fun DataItemUIPreview() {
         )
 
         Column {
-            HeaderUI()
-            for (i in mockListId.indices) {
-                ListItemUI(listId = mockListId[i], ids = mockId[i], names = mockName[i])
-                Divider()
-            }
+            ListItemUI(listIds = mockListId, ids = mockId, names = mockName)
         }
     }
 }
--- app/src/main/java/com/suryanudurupati/itemorganizer/MainActivity.kt
+++ app/src/main/java/com/suryanudurupati/itemorganizer/MainActivity.kt
@@ -8,24 +8,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.suryanudurupati.itemorganizer.ui.theme.ItemOrganizerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ItemOrganizerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
                 }
             }
         }
@@ -33,28 +34,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val mockListId = listOf(1, 2)
-    val mockId = listOf(listOf(753, 754, 755), listOf(855, 856, 857))
-    val mockName = listOf(
+fun Greeting(modifier: Modifier = Modifier) {
+    val mockListIds = listOf(1, 2)
+    val mockIds = listOf(listOf(753, 754, 755), listOf(855, 856, 857))
+    val mockNames = listOf(
         listOf("Item 753", "Item 754", "Item 755"),
         listOf("Item 855", "Item 856", "Item 857")
     )
-    Column {
-        HeaderUI()
-        Column {
-            for (i in mockListId.indices) {
-                ListItemUI(listId = mockListId[i], ids = mockId[i], names = mockName[i])
-                Divider()
-            }
-        }
-    }
+    ListItemUI(
+        listIds = mockListIds,
+        ids = mockIds,
+        names = mockNames,
+        modifier = Modifier.padding(top = 10.dp).fillMaxSize()
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ItemOrganizerTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
